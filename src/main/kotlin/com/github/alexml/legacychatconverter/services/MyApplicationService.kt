1aa3f92140edfb754cd047d13e987849d2fb7435
GitHub Action <action@github.com>
2021-11-02 22:52:56 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/alexml/legacychatconverter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.alexml.legacychatconverter.services
+
+import com.github.alexml.legacychatconverter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/alexml/legacychatconverter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alexml/legacychatconverter/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.alexml.legacychatconverter.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alexml.legacychatconverter.MyBundle
 
 class MyProjectService(project: Project) {
 
