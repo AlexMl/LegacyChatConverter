ee1669e0dde8a5b4ed99876b13fb96187baf11c3
Alexander <AlexMl@users.noreply.github.com>
2022-02-03 15:15:16 -0500

    [ci skip] change some test messages

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -24,13 +24,13 @@ public class ConverterTest {
         );
 
         Assert.assertEquals("Conversion wrong",
-                "Component.text(\"OOPS I could not send you to \", NamedTextColor.RED).append(Component.text(Core.getServerName(), NamedTextColor.RED)).append(Component.text(\"Please try again!\", NamedTextColor.RED))",
-                Converter.convertSection("ChatColor.RED + \"OOPS I could not send you to \" + Core.getServerName() + ChatColor.RED + \"Please try again!\"")
+                "Component.text(\"OOPS I could not send you to \", NamedTextColor.RED).append(Component.text(server, NamedTextColor.RED)).append(Component.text(\"Please try again!\", NamedTextColor.RED))",
+                Converter.convertSection("ChatColor.RED + \"OOPS I could not send you to \" + server + ChatColor.RED + \"Please try again!\"")
         );
 
         Assert.assertEquals("Conversion wrong",
-                "Component.text(\"OOPS\", NamedTextColor.RED).append(Component.newline()).append(Component.newline()).append(Component.text(\"I could not send you to \", NamedTextColor.RED)).append(Component.text(Core.getServerName(), NamedTextColor.RED)).append(Component.text(\"\", NamedTextColor.RED)).append(Component.newline()).append(Component.text(\"\", NamedTextColor.RED)).append(Component.text(\"Please try again!\", NamedTextColor.RED))",
-                Converter.convertSection("ChatColor.RED + \"OOPS\\n\\nI could not send you to \" + Core.getServerName() + \"\\n\" + ChatColor.RED + \"Please try again!\"")
+                "Component.text(\"OOPS\", NamedTextColor.RED).append(Component.newline()).append(Component.newline()).append(Component.text(\"I could not send you to \", NamedTextColor.RED)).append(Component.text(server, NamedTextColor.RED)).append(Component.text(\"\", NamedTextColor.RED)).append(Component.newline()).append(Component.text(\"\", NamedTextColor.RED)).append(Component.text(\"Please try again!\", NamedTextColor.RED))",
+                Converter.convertSection("ChatColor.RED + \"OOPS\\n\\nI could not send you to \" + server + \"\\n\" + ChatColor.RED + \"Please try again!\"")
         );
     }
 
@@ -47,8 +47,8 @@ public class ConverterTest {
                 Converter.convertAmpersand(" ChatColor.translateAlternateColorCodes('&', \"&7Hello World!\") ")
         );
         Assert.assertEquals("Conversion wrong",
-                "Component.text(\"➥ \", NamedTextColor.GOLD).append(Component.text(\"You must have the Manos, Gyra or Zume rank to display items with \", NamedTextColor.GRAY)).append(Component.text(\"[item]\", NamedTextColor.AQUA)).append(Component.text(\". Upgrade today at \", NamedTextColor.GRAY)).append(Component.text(\"store.mineheroes.net\", Style.style(NamedTextColor.AQUA).decorate(TextDecoration.UNDERLINED)))",
-                Converter.convertAmpersand("&6➥ &7You must have the Manos, Gyra or Zume rank to display items with &b[item]&7. Upgrade today at &b&nstore.mineheroes.net")
+                "Component.text(\"➥ \", NamedTextColor.GOLD).append(Component.text(\"You need permission to display items with \", NamedTextColor.GRAY)).append(Component.text(\"I\", NamedTextColor.AQUA)).append(Component.text(\". Upgrade today at \", NamedTextColor.GRAY)).append(Component.text(\"www.example.com\", Style.style(NamedTextColor.AQUA).decorate(TextDecoration.UNDERLINED)))",
+                Converter.convertAmpersand("&6➥ &7You need permission to display items with &bI&7. Upgrade today at &b&nwww.example.com")
         );
     }
 
@@ -77,10 +77,6 @@ public class ConverterTest {
         Assert.assertEquals("Conversion wrong",
                 "ChatColor.GREEN + ChatColor.BOLD.toString() + \"(!) \" + ChatColor.GRAY + \"You have \" + ChatColor.GREEN + \"successfully \" + ChatColor.GRAY + \"applied a \" + cursor.getItemMeta().getDisplayName()",
                 Converter.convertAmpersandToChatColor("ChatColor.translateAlternateColorCodes('&', \"&a&l(!) &7You have &asuccessfully &7applied a \" + cursor.getItemMeta().getDisplayName())"));
-        Assert.assertEquals("Conversion wrong",
-                "\"A Thank You wave has started because \" + ChatColor.GOLD + ChatColor.BOLD.toString() + \"\" + player.getPlayerName() + \" \" + ChatColor.WHITE + \"made a purchase on \" + ChatColor.GREEN + ChatColor.BOLD.toString() + \"store.mineheroes.net\"",
-                Converter.convertAmpersandToChatColor("ChatColor.translateAlternateColorCodes('&', \"A Thank You wave has started because &6&l\" + player.getPlayerName() + \" &fmade a purchase on &a&lstore.mineheroes.net\")")
-        );
         Assert.assertEquals("Conversion wrong",
                 "ChatColor.GRAY + \"Type a new name in chat (color code guide: \" + ChatColor.AQUA + ChatColor.UNDERLINE.toString() + \"ess.khhq.net/mc/\" + ChatColor.GRAY + \")\"",
                 Converter.convertAmpersandToChatColor("ChatColor.translateAlternateColorCodes('&', \"&7Type a new name in chat (color code guide: &b&ness.khhq.net/mc/&7)\")"));
